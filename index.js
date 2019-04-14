fb8e8f1944ec70e9f5d790b3da8f9f25612df262
Rafael Fragoso <rafaelfragosom@gmail.com>
2019-04-14 13:38:57 -0300

    feat: caching the urls

--- index.js
+++ index.js
@@ -2,18 +2,22 @@ import logger from 'morgan'
 import express, { Router } from 'express'
 import bodyParser from 'body-parser'
 import chalk from 'chalk'
+import apicache from 'apicache'
 import middlewares from './src/middlewares'
 import * as controller from './src/controllers'
 
 const app = express()
 const port = process.env.PORT || 3001
 const router = new Router()
+const cache = apicache.middleware
+const onlyStatus200 = (req, res) => res.statusCode === 200
+const cacheSuccesses = cache('10 days', onlyStatus200)
 
 app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-router.get('/:term', controller.search)
+router.get('/:term', cacheSuccesses, controller.search)
 app.use('/search', router)
 
 app.use(middlewares.error)
